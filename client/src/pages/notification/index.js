85231a0c348a19ef1d23e19f9e1050c45bff5836
dongxu xia <hsia1993@gmail.com>
2024-07-23 08:29:46 -0400

    fix: style

--- client/src/pages/notification/index.js
+++ client/src/pages/notification/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "../parent/KidList.css";
-import "../style.css";
 import leftArrow from "../../images/left-arrow.png";
-import profile from "../../images/user.png";
 import { notification } from "antd";
+import Navbar from "../../components/Navbar";
 
+// import "../parent/KidList.css";
+import "../style.css";
+import "./index.css";
 class KidList extends Component {
   componentDidMount() {
     let localStorage = window.localStorage;
@@ -50,17 +51,11 @@ class KidList extends Component {
     const { username, notifications } = this.state;
     return (
       <div>
-        <div className="kid-list_title_bar">
-          <Link to="/" className="kid-list__back_arrow">
-            <img src={leftArrow} alt="" />
-          </Link>
-          <label className="kid-list_title_bar_center_title">{username}</label>
-        </div>
-        <div className="kid-list__title">Notification</div>
-
+        <Navbar username={username} />
+        <div className="page-title">Notification</div>
         {notifications.map((n) => (
-          <div className="kid_list_item">
-            <label className="kid_list_top_text kid_list_item_center ">
+          <div className="notification_list_item">
+            <label className="notification_list_top_text notification_list_item_center ">
               {n.content}
             </label>
           </div>
--- client/src/pages/parent/ParentWishes.js
+++ client/src/pages/parent/ParentWishes.js
@@ -4,102 +4,132 @@ import { Link } from "react-router-dom";
 import Wallet from "../../components/Wallet";
 import "./ParentWishes.css";
 import leftArrow from "../../images/left-arrow.png";
-import finishedImg from "../../images/tick.png";
-import waitingImg from "../../images/waiting.png";
+import finishedImg from "../../images/completed.png";
+import executeBtn from "../../images/tick.png";
 import { notification } from "antd";
-import Lottie from 'lottie-react-web';
-import animation from '../../fireworks.json';
+import Lottie from "lottie-react-web";
+import animation from "../../fireworks.json";
+import Navbar from "../../components/Navbar";
 
 class ParentWishList extends Component {
-    constructor(props) {
-        super(props);
-        let localStorage = window.localStorage;
-        this.state = {
-            username: localStorage.username,
-            wishes: [],
-            congratsModalIsOpen: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    let localStorage = window.localStorage;
+    this.state = {
+      username: localStorage.username,
+      wishes: [],
+      congratsModalIsOpen: false,
+    };
+  }
 
-    componentDidMount() {
-        let localStorage = window.localStorage;
-        if (localStorage.islogin !== "1") {
-            this.props.history.replace("/login");
-        }
-        this.loadWishes();
+  componentDidMount() {
+    let localStorage = window.localStorage;
+    if (localStorage.islogin !== "1") {
+      this.props.history.replace("/login");
     }
+    this.loadWishes();
+  }
 
-    componentWillUnmount() {
-        this.setState = (state, callback) => {
-            return;
-        };
+  componentWillUnmount() {
+    this.setState = (state, callback) => {
+      return;
+    };
+  }
+
+  async loadWishes() {
+    try {
+      let result = await axios.get(`/api/parentWishes/`);
+      this.setState({
+        wishes: result.data.data,
+      });
+    } catch (e) {
+      console.log("reuslt: " + e.message);
+      notification.error({
+        message: e.response.data.msg,
+        title: "Error",
+      });
     }
+  }
 
-    async loadWishes() {
-        try {
-            let result = await axios.get(`/api/parentWishes/`);
-            this.setState({
-                wishes: result.data.data,
-            });
-            console.log("reuslt: " + JSON.stringify(result.data.data));
-        } catch (e) {
-            console.log("reuslt: " + e.message);
-            notification.error({
-                message: e.response.data.msg,
-                title: "Error",
-            });
-        }
+  // Fulfill a specific wish
+  async completeWish(wishId) {
+    console.log("Executing wish with ID:", wishId);
+    try {
+      const kidId = window.localStorage.id;
+      console.log("Kid ID:", kidId);
+      await axios.post(`/api/wish/fulfill/${wishId}`);
+      this.setState({ congratsModalIsOpen: true });
+      this.loadWishes(kidId);
+    } catch (e) {
+      notification.error({
+        message: e.response?.data?.msg || "Error fulfilling wish",
+        title: "Error",
+      });
     }
+  }
 
-    render() {
-        const { username, wishes, congratsModalIsOpen } = this.state;
-        return (
-            <div className="wish-page">
-                <div className="wish-title-bar">
-                    <Link to="/" className="wish__back-arrow">
-                        <img src={leftArrow} alt="" />
-                    </Link>
-                    <label className="wish-title-bar__center-title">{username}</label>
+  render() {
+    const { username, wishes, congratsModalIsOpen } = this.state;
+    return (
+      <div className="wish-page">
+        {/* <div className="wish-title-bar">
+          <Link to="/" className="wish__back-arrow">
+            <img src={leftArrow} alt="" />
+          </Link>
+          <label className="wish-title-bar__center-title">{username}</label>
+        </div> */}
+        <Navbar username={username} />
+
+        <div className="page-title">Manage Wishes</div>
+
+        <div className="wish-page__task-list-div">
+          <div className="wish-page__task-list">
+            {wishes.map((wish) => (
+              <div className="wish-card" key={wish._id}>
+                <div className="wish-card__content">
+                  <p className="wish-card__description">
+                    {wish.wishDescription}
+                  </p>
+                  <p className="wish-card__details">
+                    {wish.wishCost} coins |{" "}
+                    {wish.isFulfilled ? "Fulfilled" : "Pending"}
+                  </p>
                 </div>
-
-                <div className="wish-page__title">Manage Wishes</div>
-
-                <div className="wish-page__task-list-div">
-                    <div className="wish-page__task-list">
-                        {wishes.map((wish) => (
-                            <div className="wish-card" key={wish._id}>
-                                <div className="wish-card__content">
-                                    <p className="wish-card__description">{wish.wishDescription}</p>
-                                    <p className="wish-card__details">
-                                        {wish.wishCost} coins | {wish.isFulfilled ? "Fulfilled" : "Pending"}
-                                    </p>
-                                </div>
-                                <div className="wish-card__actions">
-                                    {
-                                        wish.isFulfilled ? (
-                                            <div className="wish-card__action-item">
-                                                <div className="wish-item__execute-wish-btn">
-                                                    <img src={finishedImg} alt="Completed" className="execute-wish-btn__image" />
-                                                </div>
-                                                <span className="wish-btn-label">Completed</span>
-                                            </div>
-                                        ) : (
-                                            <div className="wish-card__action-item">
-                                                <div className="wish-item__execute-wish-btn">
-                                                    <img src={waitingImg} alt="Execute" className="execute-wish-btn__image" />
-                                                </div>
-                                                <span className="wish-btn-label">On The Way</span>
-                                            </div>
-                                        )
-                                    }
-                                </div>
-                            </div>
-                        ))}
+                <div className="wish-card__actions">
+                  {wish.isFulfilled ? (
+                    <div className="wish-card__action-item">
+                      <div className="wish-item__execute-wish-btn">
+                        <img
+                          src={finishedImg}
+                          alt="Completed"
+                          className="execute-wish-btn__image"
+                        />
+                      </div>
+                      <span className="wish-btn-label">Completed</span>
                     </div>
+                  ) : (
+                    <div className="wish-card__action-item">
+                      <button
+                        className="wish-item__execute-wish-btn"
+                        onClick={() => this.completeWish(wish._id)}
+                      >
+                        <img
+                          src={executeBtn}
+                          alt="Execute"
+                          className="execute-wish-btn__image"
+                        />
+                      </button>
+                      <span className="wish-btn-label">Done</span>
+                    </div>
+                  )}
                 </div>
-            </div>
-        );
-    }
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ParentWishList;
