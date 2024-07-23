07ac4ce8ae1abca1a90392cbde0846510b3fdbd8
dongxu xia <hsia1993@gmail.com>
2024-07-23 10:13:13 -0400

    fix: kid wish list order

--- client/src/pages/kids/WishList.js
+++ client/src/pages/kids/WishList.js
@@ -7,153 +7,179 @@ import Navbar from "../../components/Navbar";
 import removeBtn from "../../images/remove.png";
 import executeBtn from "../../images/tick.png";
 import { notification } from "antd";
-import Lottie from 'lottie-react-web';
-import animation from '../../fireworks.json';
+import Lottie from "lottie-react-web";
+import animation from "../../fireworks.json";
+import dayjs from "dayjs";
 
 class WishList extends Component {
-    constructor(props) {
-        super(props);
-        let localStorage = window.localStorage;
-        this.state = {
-            username: localStorage.username,
-            wishes: [],
-            congratsModalIsOpen: false,
-        };
+  constructor(props) {
+    super(props);
+    let localStorage = window.localStorage;
+    this.state = {
+      username: localStorage.username,
+      wishes: [],
+      congratsModalIsOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    let localStorage = window.localStorage;
+    if (localStorage.islogin !== "1") {
+      this.props.history.replace("/login");
     }
+    this.loadWishes(localStorage.id);
+  }
 
-    componentDidMount() {
-        let localStorage = window.localStorage;
-        if (localStorage.islogin !== "1") {
-            this.props.history.replace("/login");
-        }
-        this.loadWishes(localStorage.id);
+  componentWillUnmount() {
+    this.setState = (state, callback) => {
+      return;
+    };
+  }
+
+  async loadWishes(id) {
+    try {
+      let result = await axios.get(`/api/kidWishes/${id}`);
+      this.setState({
+        wishes: result.data.data.sort((a, b) =>
+          dayjs(a.createTime).diff(dayjs(b.createTime))
+        ),
+      });
+    } catch (e) {
+      notification.error({
+        message: e.response.data.msg,
+        title: "Error",
+      });
     }
+  }
 
-    componentWillUnmount() {
-        this.setState = (state, callback) => {
-            return;
-        };
+  // Delete a specific wish
+  async deleteWish(wishId) {
+    console.log("Deleting wish with ID:", wishId);
+    try {
+      const kidId = window.localStorage.id;
+      console.log("Kid ID:", kidId);
+      await axios.delete(`/api/wish/${wishId}`, {
+        data: { kid: kidId },
+      });
+      notification.success({
+        message: "Wish removed successfully",
+        title: "Success",
+      });
+      this.loadWishes(kidId);
+    } catch (e) {
+      notification.error({
+        message: e.response?.data?.msg || "Error removing wish",
+        title: "Error",
+      });
     }
+  }
 
-    async loadWishes(id) {
-        try {
-            let result = await axios.get(`/api/kidWishes/${id}`);
-            this.setState({
-                wishes: result.data.data,
-            });
-        } catch (e) {
-            notification.error({
-                message: e.response.data.msg,
-                title: "Error",
-            });
-        }
+  // Fulfill a specific wish
+  async executeWish(wishId) {
+    console.log("Executing wish with ID:", wishId);
+    try {
+      const kidId = window.localStorage.id;
+      console.log("Kid ID:", kidId);
+      await axios.post(`/api/wish/fulfill/${wishId}`, {
+        kid: kidId,
+      });
+      this.setState({ congratsModalIsOpen: true });
+      this.loadWishes(kidId);
+    } catch (e) {
+      notification.error({
+        message: e.response?.data?.msg || "Error fulfilling wish",
+        title: "Error",
+      });
     }
+  }
 
-    // Delete a specific wish
-    async deleteWish(wishId) {
-        console.log("Deleting wish with ID:", wishId);
-        try {
-            const kidId = window.localStorage.id;
-            console.log("Kid ID:", kidId);
-            await axios.delete(`/api/wish/${wishId}`, {
-                data: { kid: kidId }
-            });
-            notification.success({
-                message: "Wish removed successfully",
-                title: "Success",
-            });
-            this.loadWishes(kidId);
-        } catch (e) {
-            notification.error({
-                message: e.response?.data?.msg || "Error removing wish",
-                title: "Error",
-            });
-        }
-    }
+  render() {
+    const { username, wishes, congratsModalIsOpen } = this.state;
+    return (
+      <div className="wish-page">
+        <Navbar username={username} />
 
-    // Fulfill a specific wish
-    async executeWish(wishId) {
-        console.log("Executing wish with ID:", wishId);
-        try {
-            const kidId = window.localStorage.id;
-            console.log("Kid ID:", kidId);
-            await axios.post(`/api/wish/fulfill/${wishId}`, {
-                kid: kidId
-            });
-            this.setState({ congratsModalIsOpen: true });
-            this.loadWishes(kidId);
-        } catch (e) {
-            notification.error({
-                message: e.response?.data?.msg || "Error fulfilling wish",
-                title: "Error",
-            });
-        }
-    }
+        <div className="page-title">Manage Wishes</div>
 
-    render() {
-        const { username, wishes, congratsModalIsOpen } = this.state;
-        return (
-            <div className="wish-page">
-                <Navbar username={username} />
+        <Wallet />
 
-                <div className="page-title">Manage Wishes</div>
+        {congratsModalIsOpen ? (
+          <div className="congrats-modal">
+            <Lottie
+              options={{
+                animationData: animation,
+                loop: false,
+              }}
+            />
+            <p className="congrats-modal__text">
+              YOUR WISH'S <br />
+              COME TRUE!!!
+            </p>
+            <button
+              className="style_common_button style_common_button_purple congrats-modal__close-btn"
+              onClick={() => this.setState({ congratsModalIsOpen: false })}
+            >
+              Close
+            </button>
+          </div>
+        ) : null}
 
-                <Wallet />
+        <div className="wish-page__add-wish-button-div">
+          <Link to="/wishadd" className="wish-page__add-wish-button">
+            Add Wish
+          </Link>
+        </div>
 
-                {congratsModalIsOpen ? (
-                    <div className="congrats-modal">
-                        <Lottie
-                            options={{
-                                animationData: animation,
-                                loop: false
-                            }}
+        <div className="wish-page__wish-list-div">
+          <div className="wish-page__wish-list">
+            {wishes.map((wish) => (
+              <div className="wish-card" key={wish._id}>
+                <div className="wish-card__content">
+                  <p className="wish-card__description">
+                    {wish.wishDescription}
+                  </p>
+                  <p className="wish-card__details">
+                    {wish.wishCost} coins |{" "}
+                    {wish.isFulfilled ? "Fulfilled" : "Pending"}
+                  </p>
+                </div>
+                <div className="wish-card__actions">
+                  {!wish.isFulfilled && (
+                    <div className="wish-card__action-item">
+                      <button
+                        className="wish-item__execute-wish-btn"
+                        onClick={() => this.executeWish(wish._id)}
+                      >
+                        <img
+                          src={executeBtn}
+                          alt="Execute"
+                          className="execute-wish-btn__image"
                         />
-                        <p className="congrats-modal__text">YOUR WISH'S <br />COME TRUE!!!</p>
-                        <button className="style_common_button style_common_button_purple congrats-modal__close-btn" onClick={() => this.setState({ congratsModalIsOpen: false })}>
-                            Close
-                        </button>
+                      </button>
+                      <span className="wish-btn-label done">Done</span>
                     </div>
-                ) : null}
-
-                <div className="wish-page__add-wish-button-div">
-                    <Link to="/wishadd" className="wish-page__add-wish-button">
-                        Add Wish
-                    </Link>
+                  )}
+                  <div className="wish-card__action-item">
+                    <button
+                      className="wish-item__remove-from-list"
+                      onClick={() => this.deleteWish(wish._id)}
+                    >
+                      <img
+                        src={removeBtn}
+                        alt="Remove"
+                        className="remove-btn__image"
+                      />
+                    </button>
+                    <span className="wish-btn-label delete">Delete</span>
+                  </div>
                 </div>
-
-                <div className="wish-page__wish-list-div">
-                    <div className="wish-page__wish-list">
-                        {wishes.map((wish) => (
-                            <div className="wish-card" key={wish._id}>
-                                <div className="wish-card__content">
-                                    <p className="wish-card__description">{wish.wishDescription}</p>
-                                    <p className="wish-card__details">
-                                        {wish.wishCost} coins | {wish.isFulfilled ? "Fulfilled" : "Pending"}
-                                    </p>
-                                </div>
-                                <div className="wish-card__actions">
-                                    {!wish.isFulfilled && (
-                                        <div className="wish-card__action-item">
-                                            <button className="wish-item__execute-wish-btn" onClick={() => this.executeWish(wish._id)}>
-                                                <img src={executeBtn} alt="Execute" className="execute-wish-btn__image" />
-                                            </button>
-                                            <span className="wish-btn-label done">Done</span>
-                                        </div>
-                                    )}
-                                    <div className="wish-card__action-item">
-                                        <button className="wish-item__remove-from-list" onClick={() => this.deleteWish(wish._id)}>
-                                            <img src={removeBtn} alt="Remove" className="remove-btn__image" />
-                                        </button>
-                                        <span className="wish-btn-label delete">Delete</span>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        );
-    }
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default WishList;
